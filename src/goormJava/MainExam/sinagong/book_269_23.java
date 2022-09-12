0b30440efbe9f95cb9cdf9bb121554b0861a11fc
yujisu01 <jssla1@naver.com>
2022-09-12 16:52:38 +0900

    220912

---
+++ src/goormJava/MainExam/sinagong/book_269_23.java
@@ -0,0 +1,9 @@
+package goormJava.MainExam.sinagong;
+
+public class book_269_23 {
+	public static void main(String[] args) {
+		String str;
+		str="Power Overwhelming!";
+				System.out.printf("%8.10s\n",str);
+	}
+}
