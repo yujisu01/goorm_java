1be878f4114c1f8ffbd8add80c14e4f72ada941c
yujisu01 <jssla1@naver.com>
2022-10-07 21:41:47 +0900

    1007

---
+++ src/goormJava/MainExam/book/book14_096_34.java
@@ -0,0 +1,15 @@
+package goormJava.MainExam.book;
+
+public class book14_096_34 {
+	public static void main(String[] args) {
+		int a=2; 
+		int b;
+		while(a-- > 0) {
+			System.out.printf("a=%d\n",a);
+		}
+		for(b=0; b<2; b++) {
+			System.out.printf("a=%d\n", a++);
+		}
+	}
+
+}
